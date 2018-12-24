84c6c6dac60db758d7cc0bfd27d1d9101770b546
sherylynn <352281674@qq.com>
2018-12-24 14:10:34 +0800

    add compatible to python 2 and python3

--- xingseapi.py
+++ xingseapi.py
@@ -1,8 +1,22 @@
 # coding=utf-8
 import base64
-import urllib, urllib2, sys
 import ssl
+import sys
 
+IS_PY3 = sys.version_info.major == 3
+if IS_PY3:
+    from urllib.request import urlopen
+    from urllib.request import Request
+    from urllib.error import URLError
+    from urllib.parse import urlencode
+    from urllib.parse import quote_plus
+else:
+    import urllib2
+    from urllib import quote_plus
+    from urllib2 import urlopen
+    from urllib2 import Request
+    from urllib2 import URLError
+    from urllib import urlencode
 
 host = 'https://plantapi.xingseapp.com'
 path = '/item/identification'
@@ -15,15 +29,15 @@ url = host + path
 with open(u"./pics/杜鹃.jpeg", "rb") as image_file:
 
     bodys['image_data'] = base64.b64encode(image_file.read()).decode('ascii')
-    post_data = urllib.urlencode(bodys)
-    request = urllib2.Request(url, post_data)
+    post_data = urlencode(bodys).encode("utf-8")
+    request = Request(url, post_data)
     request.add_header('Authorization', 'APPCODE ' + appcode)
     ##根据API的要求，定义相对应的Content-Type
     request.add_header('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8')
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
-    response = urllib2.urlopen(request, context=ctx)
+    response = urlopen(request, context=ctx)
     content = response.read()
     if (content):
         print(content)
